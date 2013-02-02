7fb55e87099c9e6a48cdf1266fa1b98658269aed
tnantoka <bornneet@livedoor.com>
2013-02-02 15:32:45 +0900

    Initial Commit

---
+++ ArrayControllerPracticeTests/ArrayControllerPracticeTests.h
@@ -0,0 +1,13 @@
+//
+//  ArrayControllerPracticeTests.h
+//  ArrayControllerPracticeTests
+//
+//  Created by Tatsuya Tobioka on 13/02/02.
+//  Copyright (c) 2013年 Tatsuya Tobioka. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ArrayControllerPracticeTests : SenTestCase
+
+@end
---
+++ ArrayControllerPracticeTests/ArrayControllerPracticeTests.m
@@ -0,0 +1,32 @@
+//
+//  ArrayControllerPracticeTests.m
+//  ArrayControllerPracticeTests
+//
+//  Created by Tatsuya Tobioka on 13/02/02.
+//  Copyright (c) 2013年 Tatsuya Tobioka. All rights reserved.
+//
+
+#import "ArrayControllerPracticeTests.h"
+
+@implementation ArrayControllerPracticeTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ArrayControllerPracticeTests");
+}
+
+@end
